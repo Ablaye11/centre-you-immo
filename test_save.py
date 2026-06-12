776a71d8d86e20840b3cd7ec67a4393be2e8605c
Your Name <you@example.com>
2026-06-12 21:12:33 +0000

    debug: add diagnostic script to profile user.save()

---
+++ test_save.py
@@ -0,0 +1,17 @@
+import os
+import logging
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'youimmo.settings')
+django.setup()
+
+# Activer le log des requêtes SQL dans la console
+logger = logging.getLogger('django.db.backends')
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler())
+
+from django.contrib.auth.models import User
+u = User.objects.first()
+print('--- Récupération du User OK, début du save() ---')
+u.save()
+print('--- save() OK ---')
