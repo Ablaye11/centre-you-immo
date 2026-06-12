6b0634fce28b3d9dae0f524f2dc1f69383aa653b
Your Name <you@example.com>
2026-06-12 21:20:06 +0000

    debug: force DEBUG=True in diagnostic script to print SQL queries

--- test_save.py
+++ test_save.py
@@ -5,6 +5,9 @@ import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'youimmo.settings')
 django.setup()
 
+from django.conf import settings
+settings.DEBUG = True
+
 # Activer le log des requêtes SQL dans la console
 logger = logging.getLogger('django.db.backends')
 logger.setLevel(logging.DEBUG)
